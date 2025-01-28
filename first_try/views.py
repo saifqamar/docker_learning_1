1c01d0af40f216ba50e06bdeb993af04fa85111e
saif qamar <adlyaqeen2@gmail.com>
2025-01-29 01:17:34 +0500

    just a change

--- first_try/views.py
+++ first_try/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello, Worlddddd!")
 
 
 def second(request):
