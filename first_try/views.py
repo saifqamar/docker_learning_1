96a91a6eacd3770fbeb2305deed4f183774817e6
saif qamar <adlyaqeen2@gmail.com>
2025-01-29 01:30:10 +0500

    aljd

--- first_try/views.py
+++ first_try/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, Worlddddd!")
+    return HttpResponse("Hello, World!")
 
 
 def second(request):
